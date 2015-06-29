c57644de1e7408749ad271a8468d6ea8a0bbb7cb
Taro YABUKI <jr.tr.ybk@gmail.com>
2015-06-29 18:44:41 +0900

    第11回

--- lecture/10/tictactoe.js
+++ lecture/10/tictactoe.js
@@ -11,8 +11,9 @@ function クリックへの反応(位置) {
     盤面[位置] = -1;
   }
   console.log(盤面);
-  var 結果 = 揃ったか();
-  console.log('揃った：' + 結果);
+  
+  var 結果 = そろったか();
+  console.log('そろった：' + 結果);
   
   if (結果 == 1) {
     $('div').addClass('終');
@@ -22,12 +23,9 @@ function クリックへの反応(位置) {
   }
 }
 
-function 揃ったか() {
+function そろったか() {
   if (盤面[0] == 1 && 盤面[1] == 1 && 盤面[2] == 1) {
     return 1;
   }
-  if (盤面[0] == -1 && 盤面[1] == -1 && 盤面[2] == -1) {
-    return -1;
-  }
-  return null;
+  return 0;
 }
